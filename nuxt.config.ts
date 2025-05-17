a9581b0463fa5b8fc54feaaf77ddb9c107fb7464
Iceship <iceship@gmail.com>
2025-05-17 22:09:30 +0900

    :sparkles: eslint tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
+  css: ["~/assets/css/main.css"],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  colorMode: {
+    dataValue: "theme",
+  },
+});
